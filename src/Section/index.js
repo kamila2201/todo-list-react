de795349c7c0bd30464d2b98c33755d335a2d53e
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-02-28 22:40:34 +0100

    task list without interactions yet

---
+++ src/Section/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const Section = ({header, body, extraHeaderContent}) => (
+    <section className="section">
+            <div className="section__container">
+            <h2 className="section__header">{header}</h2>
+            {extraHeaderContent}
+        </div>
+            <div className="section__body">
+                {body}
+            </div>
+        </section>
+);
+
+export default Section;
