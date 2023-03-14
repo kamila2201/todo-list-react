60dafa8e7427d83e6546208df32e03fb90cf81d3
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-03-14 17:07:09 +0100

    adding ne task interactions

--- src/App.js
+++ src/App.js
@@ -37,12 +37,23 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         header="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask}/>}
       />
       <Section
         header="Lista zadań"
