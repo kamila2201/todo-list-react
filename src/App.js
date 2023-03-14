bbf21bdf68f9e5ce363e732994356afc40ef98bf
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-03-14 15:20:18 +0100

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done }
+      }
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -31,12 +40,13 @@ function App() {
       <Section
         header="Lista zadań"
         body={
-        <Tasks 
-        tasks={tasks} 
-        hideDone={hideDone} 
-        removeTask={removeTask}
-        />
-      }
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
         extraHeaderContent={
           <Buttons
             tasks={tasks}
