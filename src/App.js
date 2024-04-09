50f2056d79ca0970a420b2901b1450b6bcd45541
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-09 20:12:44 +0200

    empty task list

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "być szczęśliwa", done: false },
-    { id: 2, content: "zrobić zadanie", done: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
