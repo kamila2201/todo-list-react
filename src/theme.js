97ed98f2bd2b3573701f8b7bcf6f70bfed30bc51
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-04-13 21:46:56 +0200

    created theme.js with an object containing guiding colors and an object with a breakpoint for mobile phones

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        black: "#000",
+        silver: "#ccc",
+        white: "#fff",
+        gallery: "#eee",
+        alto: "#ddd",
+        teal: "#008080",
+        red: "#FF0000",
+        green: "#008000",
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
